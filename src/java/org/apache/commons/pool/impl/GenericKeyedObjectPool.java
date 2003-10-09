bc9a57841e2dc875f18d2a24b50573700400ef48
Robert Burrell Donkin <rdonkin@apache.org>
2003-10-09 21:45:57 +0000

    Fixed typos in apache licenses
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/pool/trunk@139735 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/pool/impl/GenericKeyedObjectPool.java
+++ src/java/org/apache/commons/pool/impl/GenericKeyedObjectPool.java
@@ -1,7 +1,7 @@
 /*
  * $Source: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//pool/src/java/org/apache/commons/pool/impl/GenericKeyedObjectPool.java,v $
- * $Revision: 1.23 $
- * $Date: 2003/08/27 10:45:57 $
+ * $Revision: 1.24 $
+ * $Date: 2003/10/09 21:45:56 $
  *
  * ====================================================================
  *
@@ -26,8 +26,8 @@
  *    any, must include the following acknowledgement:
  *       "This product includes software developed by the
  *        Apache Software Foundation - http://www.apache.org/"
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
+ *    Alternately, this acknowledgement may appear in the software itself,
+ *    if and wherever such third-party acknowledgements normally appear.
  *
  * 4. The names "The Jakarta Project", "Commons", and "Apache Software
  *    Foundation" must not be used to endorse or promote products derived
@@ -36,7 +36,7 @@
  *
  * 5. Products derived from this software may not be called "Apache"
  *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
+ *    permission of the Apache Software Foundation.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -165,7 +165,7 @@ import org.apache.commons.pool.KeyedPoolableObjectFactory;
  * @see GenericObjectPool
  * @author Rodney Waldhoff
  * @author Dirk Verbeeck
- * @version $Revision: 1.23 $ $Date: 2003/08/27 10:45:57 $
+ * @version $Revision: 1.24 $ $Date: 2003/10/09 21:45:56 $
  */
 public class GenericKeyedObjectPool extends BaseKeyedObjectPool implements KeyedObjectPool {
 
