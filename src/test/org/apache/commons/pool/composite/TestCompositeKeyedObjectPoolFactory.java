fd47e1af5ffa9435750e213a776766b6fa99b7ce
Sandy McArthur Jr <sandymac@apache.org>
2006-04-02 08:00:47 +0000

    More unit tests.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/pool/trunk@390797 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/pool/composite/TestCompositeKeyedObjectPoolFactory.java
+++ src/test/org/apache/commons/pool/composite/TestCompositeKeyedObjectPoolFactory.java
@@ -16,11 +16,17 @@
 
 package org.apache.commons.pool.composite;
 
-import org.apache.commons.pool.TestKeyedObjectPoolFactory;
-import org.apache.commons.pool.KeyedObjectPoolFactory;
-import org.apache.commons.pool.KeyedPoolableObjectFactory;
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import org.apache.commons.pool.KeyedObjectPoolFactory;
+import org.apache.commons.pool.KeyedPoolableObjectFactory;
+import org.apache.commons.pool.MethodCallPoolableObjectFactory;
+import org.apache.commons.pool.PoolUtils;
+import org.apache.commons.pool.PoolableObjectFactory;
+import org.apache.commons.pool.TestKeyedObjectPoolFactory;
+import org.apache.commons.pool.impl.StackObjectPoolFactory;
+
+import java.util.Random;
 
 /**
  * Tests for {@link CompositeKeyedObjectPoolFactory}.
@@ -29,6 +35,8 @@ import junit.framework.TestSuite;
  * @version $Revision$ $Date$
  */
 public class TestCompositeKeyedObjectPoolFactory extends TestKeyedObjectPoolFactory {
+    private final Random rnd = new Random();
+
     public TestCompositeKeyedObjectPoolFactory(final String name) {
         super(name);
     }
@@ -39,5 +47,99 @@ public class TestCompositeKeyedObjectPoolFactory extends TestKeyedObjectPoolFact
 
     protected KeyedObjectPoolFactory makeFactory(final KeyedPoolableObjectFactory objectFactory) throws UnsupportedOperationException {
         return new CompositeKeyedObjectPoolFactory(objectFactory);
+    }
+
+    public void testConstructors() throws Exception {
+        CompositeKeyedObjectPoolFactory factory = new CompositeKeyedObjectPoolFactory(createObjectFactory());
+        factory.createPool().close();
+
+        try {
+            new CompositeKeyedObjectPoolFactory((KeyedPoolableObjectFactory)null);
+            fail();
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+
+        try {
+            new CompositeKeyedObjectPoolFactory((CompositeObjectPoolFactory)null);
+            fail();
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+        factory = new CompositeKeyedObjectPoolFactory(new CompositeObjectPoolFactory(new MethodCallPoolableObjectFactory()));
+        factory.createPool().close();
+
+        try {
+            new CompositeKeyedObjectPoolFactory((PoolableObjectFactory)null);
+            fail();
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+        factory = new CompositeKeyedObjectPoolFactory(new MethodCallPoolableObjectFactory());
+        factory.createPool().close();
+    }
+
+    public void testStaticCreatePool() {
+        assertNotNull(CompositeKeyedObjectPoolFactory.createPool(new StackObjectPoolFactory()));
+        try {
+            CompositeKeyedObjectPoolFactory.createPool(null);
+            fail();
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+    }
+
+    public void testFactory() {
+        CompositeKeyedObjectPoolFactory factory = (CompositeKeyedObjectPoolFactory)makeFactory();
+        try {
+            factory.setFactory(null);
+            fail();
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+
+        factory.setFactory(new MethodCallPoolableObjectFactory());
+        assertNotNull(factory.getFactory());
+        assertNull(factory.getKeyedFactory());
+    }
+
+    public void testKeyedFactory() {
+        CompositeKeyedObjectPoolFactory factory = (CompositeKeyedObjectPoolFactory)makeFactory();
+        try {
+            factory.setKeyedFactory(null);
+            fail();
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+
+        factory.setKeyedFactory(PoolUtils.adapt(new MethodCallPoolableObjectFactory()));
+        assertNotNull(factory.getKeyedFactory());
+        assertNull(factory.getFactory());
+    }
+
+    public void testClone() throws CloneNotSupportedException {
+        CompositeKeyedObjectPoolFactory f1 = (CompositeKeyedObjectPoolFactory)makeFactory();
+        f1.setBorrowPolicy(BorrowPolicy.SOFT_FIFO);
+        f1.setEvictIdleMillis(rnd.nextLong());
+        f1.setEvictInvalidFrequencyMillis(rnd.nextLong());
+        f1.setExhaustionPolicy(ExhaustionPolicy.FAIL);
+        f1.setLimitPolicy(LimitPolicy.FAIL);
+        f1.setMaxActivePerKey(rnd.nextInt());
+        f1.setMaxIdlePerKey(rnd.nextInt());
+        f1.setMaxWaitMillis(rnd.nextInt());
+        f1.setTrackingPolicy(TrackingPolicy.REFERENCE);
+        f1.setValidateOnReturn(rnd.nextBoolean());
+        CompositeKeyedObjectPoolFactory f2 = (CompositeKeyedObjectPoolFactory)f1.clone();
+
+        assertEquals(f1.getBorrowPolicy(), f2.getBorrowPolicy());
+        assertEquals(f1.getEvictIdleMillis(), f2.getEvictIdleMillis());
+        assertEquals(f1.getEvictInvalidFrequencyMillis(), f2.getEvictInvalidFrequencyMillis());
+        assertEquals(f1.getExhaustionPolicy(), f2.getExhaustionPolicy());
+        assertEquals(f1.getLimitPolicy(), f2.getLimitPolicy());
+        assertEquals(f1.getMaxActivePerKey(), f2.getMaxActivePerKey());
+        assertEquals(f1.getMaxIdlePerKey(), f2.getMaxIdlePerKey());
+        assertEquals(f1.getMaxWaitMillis(), f2.getMaxWaitMillis());
+        assertEquals(f1.getTrackingPolicy(), f2.getTrackingPolicy());
+        assertEquals(f1.isValidateOnReturn(), f2.isValidateOnReturn());
     }
 }
