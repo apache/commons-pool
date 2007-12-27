apache/commons-pool

commit 0bc3c5496d2a06a722d150467d1dbef816edf6a5

Created until tests to verify ObjectPools conform to new contracts. Refactored ObjectPool implementations to comply with new contracts.
git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/pool/trunk@385746 13f79535-47bb-0310-9956-ffa450edef68

src/java/org/apache/commons/pool/impl/GenericObjectPool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
+ * Copyright 1999-2004,2006 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -123,6 +123,7 @@ import org.apache.commons.pool.impl.GenericKeyedObjectPool.ObjectTimestampPair;
  * @see GenericKeyedObjectPool
  * @author Rodney Waldhoff
  * @author Dirk Verbeeck
+ * @author Sandy McArthur
  * @version $Revision$ $Date$
  */
 public class GenericObjectPool extends BaseObjectPool implements ObjectPool {
@@ -211,7 +212,7 @@ public class GenericObjectPool extends BaseObjectPool implements ObjectPool {
      * @see #getTestOnBorrow
      * @see #setTestOnBorrow
      */
-    public static final boolean DEFAULT_TEST_ON_BORROW = false;
+    public static final boolean DEFAULT_TEST_ON_BORROW = true;
 
     /**
      * The default "test on return" value.
@@ -840,6 +841,7 @@ public class GenericObjectPool extends BaseObjectPool implements ObjectPool {
                     Object obj = _factory.makeObject();
                     pair = new ObjectTimestampPair(obj);
                     newlyCreated = true;
+                    return pair.value;
                 } finally {
                     if (!newlyCreated) {
                         // object cannot be created
@@ -878,18 +880,22 @@ public class GenericObjectPool extends BaseObjectPool implements ObjectPool {
     }
 
     public synchronized void invalidateObject(Object obj) throws Exception {
-        assertOpen();
         try {
-            _factory.destroyObject(obj);
-        }
-        finally {
+            if (_factory != null) {
+                _factory.destroyObject(obj);
+            }
+        } catch (Exception e) {
+            // swallowed
+        } finally {
             _numActive--;
             notifyAll(); // _numActive has changed
         }
     }
 
+    /**
+     * Clears any objects sitting idle in the pool.
+     */
     public synchronized void clear() {
-        assertOpen();
         for(Iterator it = _pool.iterator(); it.hasNext(); ) {
             try {
                 _factory.destroyObject(((ObjectTimestampPair)(it.next())).value);
@@ -902,31 +908,46 @@ public class GenericObjectPool extends BaseObjectPool implements ObjectPool {
         notifyAll(); // num sleeping has changed
     }
 
+    /**
+     * Return the number of instances currently borrowed from this pool.
+     *
+     * @return the number of instances currently borrowed from this pool
+     */
     public synchronized int getNumActive() {
-        assertOpen();
         return _numActive;
     }
 
+    /**
+     * Return the number of instances currently idle in this pool.
+     *
+     * @return the number of instances currently idle in this pool
+     */
     public synchronized int getNumIdle() {
-        assertOpen();
         return _pool.size();
     }
 
     public synchronized void returnObject(Object obj) throws Exception {
-        assertOpen();
-        addObjectToPool(obj, true);
+        try {
+            addObjectToPool(obj, true);
+        } catch (Exception e) {
+            if (_factory != null) {
+                try {
+                    _factory.destroyObject(obj);
+                } catch (Exception e2) {
+                    // swallowed
+                }
+            }
+        }
     }
 
     private void addObjectToPool(Object obj, boolean decrementNumActive) throws Exception {
-        boolean success = true;
+        boolean success;
         if(_testOnReturn && !(_factory.validateObject(obj))) {
             success = false;
         } else {
-            try {
-                _factory.passivateObject(obj);
-            } catch(Exception e) {
-                success = false;
-            }
+            success = false;
+            _factory.passivateObject(obj);
+            success = !isClosed();
         }
 
         boolean shouldDestroy = !success;
@@ -942,7 +963,7 @@ public class GenericObjectPool extends BaseObjectPool implements ObjectPool {
         notifyAll(); // _numActive has changed
 
         if(shouldDestroy) {
-            try {
+            try { // TODO: remove try/catch
                 _factory.destroyObject(obj);
             } catch(Exception e) {
                 // ignored
@@ -952,12 +973,19 @@ public class GenericObjectPool extends BaseObjectPool implements ObjectPool {
 
     public synchronized void close() throws Exception {
         clear();
-        _pool = null;
-        _factory = null;
         startEvictor(-1L);
         super.close();
     }
 
+    /**
+     * Sets the {@link PoolableObjectFactory factory} this pool uses
+     * to create new instances. Trying to change
+     * the <code>factory</code> while there are borrowed objects will
+     * throw an {@link IllegalStateException}.
+     *
+     * @param factory the {@link PoolableObjectFactory} used to create new instances.
+     * @throws IllegalStateException when the factory cannot be set at this time
+     */
     public synchronized void setFactory(PoolableObjectFactory factory) throws IllegalStateException {
         assertOpen();
         if(0 < getNumActive()) {

src/java/org/apache/commons/pool/KeyedObjectPool.java
@@ -138,11 +138,11 @@ public interface KeyedObjectPool {
     /**
      * Returns the number of instances
      * corresponding to the given <code>key</code>
-     * currently idle in my pool (optional operation).
+     * currently idle in this pool (optional operation).
      * Returns a negative value if this information is not available.
      *
      * @param key the key to query
-     * @return the number of instances corresponding to the given <code>key</code> currently idle in my pool or a negative value if unsupported
+     * @return the number of instances corresponding to the given <code>key</code> currently idle in this pool or a negative value if unsupported
      * @throws UnsupportedOperationException <strong>deprecated</strong>: when this implementation doesn't support the operation
      */
     int getNumIdle(Object key) throws UnsupportedOperationException;
@@ -155,28 +155,28 @@ public interface KeyedObjectPool {
      * Returns a negative value if this information is not available.
      *
      * @param key the key to query
-     * @return the number of instances corresponding to the given <code>key</code> currently borrowed in my pool or a negative value if unsupported
+     * @return the number of instances corresponding to the given <code>key</code> currently borrowed in this pool or a negative value if unsupported
      * @throws UnsupportedOperationException <strong>deprecated</strong>: when this implementation doesn't support the operation
      */
     int getNumActive(Object key) throws UnsupportedOperationException;
 
     /**
      * Returns the total number of instances
-     * currently idle in my pool (optional operation).
+     * currently idle in this pool (optional operation).
      * Returns a negative value if this information is not available.
      *
-     * @return the total number of instances currently idle in my pool or a negative value if unsupported
+     * @return the total number of instances currently idle in this pool or a negative value if unsupported
      * @throws UnsupportedOperationException <strong>deprecated</strong>: when this implementation doesn't support the operation
      */
     int getNumIdle() throws UnsupportedOperationException;
 
     /**
      * Returns the total number of instances
-     * current borrowed from my pool but not
+     * current borrowed from this pool but not
      * yet returned (optional operation).
      * Returns a negative value if this information is not available.
      *
-     * @return the total number of instances currently borrowed from my pool or a negative value if unsupported
+     * @return the total number of instances currently borrowed from this pool or a negative value if unsupported
      * @throws UnsupportedOperationException <strong>deprecated</strong>: when this implementation doesn't support the operation
      */
     int getNumActive() throws UnsupportedOperationException;

src/java/org/apache/commons/pool/ObjectPool.java
@@ -50,7 +50,7 @@ import java.util.NoSuchElementException;
  *
  * @author Rodney Waldhoff
  * @author Sandy McArthur
- * @version $Revision$ $Date$ 
+ * @version $Revision$ $Date$
  * @see PoolableObjectFactory
  * @see ObjectPoolFactory
  * @see KeyedObjectPool
@@ -124,24 +124,24 @@ public interface ObjectPool {
 
     /**
      * Return the number of instances
-     * currently idle in my pool (optional operation).
+     * currently idle in this pool (optional operation).
      * This may be considered an approximation of the number
      * of objects that can be {@link #borrowObject borrowed}
      * without creating any new instances.
      * Returns a negative value if this information is not available.
      *
-     * @return the number of instances currently idle in my pool or a negative value if unsupported
+     * @return the number of instances currently idle in this pool or a negative value if unsupported
      * @throws UnsupportedOperationException <strong>deprecated</strong>: if this implementation does not support the operation
      */
     int getNumIdle() throws UnsupportedOperationException;
 
     /**
      * Return the number of instances
-     * currently borrowed from my pool 
+     * currently borrowed from this pool
      * (optional operation).
      * Returns a negative value if this information is not available.
      *
-     * @return the number of instances currently borrowed in my pool or a negative value if unsupported
+     * @return the number of instances currently borrowed from this pool or a negative value if unsupported
      * @throws UnsupportedOperationException <strong>deprecated</strong>: if this implementation does not support the operation
      */
     int getNumActive() throws UnsupportedOperationException;
@@ -149,6 +149,7 @@ public interface ObjectPool {
     /**
      * Clears any objects sitting idle in the pool, releasing any
      * associated resources (optional operation).
+     * Idle objects cleared must be {@link PoolableObjectFactory#destroyObject(Object) destroyed}.
      *
      * @throws UnsupportedOperationException if this implementation does not support the operation
      */
@@ -173,7 +174,7 @@ public interface ObjectPool {
      * throw an {@link UnsupportedOperationException}. It is up to the pool
      * implementation to determine when it is acceptable to call this method.
      *
-     * @param factory the {@link PoolableObjectFactory} I use to create new instances.
+     * @param factory the {@link PoolableObjectFactory} used to create new instances.
      * @throws IllegalStateException when the factory cannot be set at this time
      * @throws UnsupportedOperationException if this implementation does not support the operation
      */

src/java/org/apache/commons/pool/impl/SoftReferenceObjectPool.java
@@ -27,6 +27,7 @@ import java.util.NoSuchElementException;
 import org.apache.commons.pool.BaseObjectPool;
 import org.apache.commons.pool.ObjectPool;
 import org.apache.commons.pool.PoolableObjectFactory;
+import org.apache.commons.pool.PoolUtils;
 
 /**
  * A {@link java.lang.ref.SoftReference SoftReference} based
@@ -37,36 +38,57 @@ import org.apache.commons.pool.PoolableObjectFactory;
  * @version $Revision$ $Date$
  */
 public class SoftReferenceObjectPool extends BaseObjectPool implements ObjectPool {
+    /**
+     * Create a <code>SoftReferenceObjectPool</code> without a factory.
+     * {@link #setFactory(PoolableObjectFactory) setFactory} should be called
+     * before any attempts to use the pool are made.
+     * Generally speaking you should prefer the {@link #SoftReferenceObjectPool(PoolableObjectFactory)} constructor.
+     *
+     * @see #SoftReferenceObjectPool(PoolableObjectFactory)
+     */
     public SoftReferenceObjectPool() {
         _pool = new ArrayList();
         _factory = null;
     }
 
+    /**
+     * Create a <code>SoftReferenceObjectPool</code> with the specified factory.
+     *
+     * @param factory object factory to use.
+     */
     public SoftReferenceObjectPool(PoolableObjectFactory factory) {
         _pool = new ArrayList();
         _factory = factory;
     }
 
-    public SoftReferenceObjectPool(PoolableObjectFactory factory, int initSize) throws Exception {
-        _pool = new ArrayList();
-        _factory = factory;
-        if(null != _factory) {
-            for(int i=0;i<initSize;i++) {
-                Object obj = _factory.makeObject();
-                _factory.passivateObject(obj);
-                _pool.add(new SoftReference(obj, refQueue));
-            }
+    /**
+     * Create a <code>SoftReferenceObjectPool</code> with the specified factory and initial idle object count.
+     *
+     * @param factory object factory to use.
+     * @throws Exception when there is a problem prefilling the pool.
+     * @throws IllegalArgumentException when <code>factory</code> is <code>null</code>.
+     * @deprecated because this is a SoftReference pool, prefilled idle obejects may be garbage collected before they are used.
+     *      To be removed in Pool 3.0.
+     */
+    public SoftReferenceObjectPool(PoolableObjectFactory factory, int initSize) throws Exception, IllegalArgumentException {
+        if (factory == null) {
+            throw new IllegalArgumentException("factory required to prefill the pool.");
         }
+        _pool = new ArrayList(initSize);
+        _factory = factory;
+        PoolUtils.prefill(this, initSize);
     }
 
-    public synchronized Object borrowObject() throws Exception {        
+    public synchronized Object borrowObject() throws Exception {
         assertOpen();
         Object obj = null;
+        boolean newlyCreated = false;
         while(null == obj) {
             if(_pool.isEmpty()) {
                 if(null == _factory) {
                     throw new NoSuchElementException();
                 } else {
+                    newlyCreated = true;
                     obj = _factory.makeObject();
                 }
             } else {
@@ -74,12 +96,35 @@ public class SoftReferenceObjectPool extends BaseObjectPool implements ObjectPoo
                 obj = ref.get();
                 ref.clear(); // prevent this ref from being enqueued with refQueue.
             }
-            if(null != _factory && null != obj) {
-                _factory.activateObject(obj);
+            if (!newlyCreated && null != _factory && null != obj) {
+                try {
+                    _factory.activateObject(obj);
+                } catch (Exception e) {
+                    try {
+                        _factory.destroyObject(obj);
+                    } catch (Exception e2) {
+                        // swallowed
+                    } finally {
+                        obj = null;
+                    }
+                }
             }
-            if (null != _factory && null != obj && !_factory.validateObject(obj)) {
-                _factory.destroyObject(obj);
-                obj = null;
+            if (!newlyCreated && null != _factory && null != obj) {
+                boolean validated = false;
+                try {
+                    validated = _factory.validateObject(obj);
+                } catch (Exception e) {
+                    // swallowed
+                }
+                if (!validated) {
+                    try {
+                        _factory.destroyObject(obj);
+                    } catch(Exception e) {
+                        // swallowed
+                    } finally {
+                        obj = null;
+                    }
+                }
             }
         }
         _numActive++;
@@ -87,11 +132,8 @@ public class SoftReferenceObjectPool extends BaseObjectPool implements ObjectPoo
     }
 
     public synchronized void returnObject(Object obj) throws Exception {
-        assertOpen();
-        boolean success = true;
-        if(!(_factory.validateObject(obj))) {
-            success = false;
-        } else {
+        boolean success = !isClosed();
+        if (_factory != null) {
             try {
                 _factory.passivateObject(obj);
             } catch(Exception e) {
@@ -106,20 +148,24 @@ public class SoftReferenceObjectPool extends BaseObjectPool implements ObjectPoo
         }
         notifyAll(); // _numActive has changed
 
-        if(shouldDestroy) {
+        if (shouldDestroy && _factory != null) {
             try {
                 _factory.destroyObject(obj);
             } catch(Exception e) {
                 // ignored
             }
         }
-
     }
 
     public synchronized void invalidateObject(Object obj) throws Exception {
-        assertOpen();
         _numActive--;
-        _factory.destroyObject(obj);
+        if (_factory != null) {
+            try {
+                _factory.destroyObject(obj);
+            } catch (Exception e) {
+                // swallowed
+            }
+        }
         notifyAll(); // _numActive has changed
     }
 
@@ -130,24 +176,44 @@ public class SoftReferenceObjectPool extends BaseObjectPool implements ObjectPoo
     public synchronized void addObject() throws Exception {
         assertOpen();
         Object obj = _factory.makeObject();
-        _numActive++;   // A little slimy - must do this because returnObject decrements it.
-        returnObject(obj);
+
+        boolean success = true;
+        _factory.passivateObject(obj);
+
+        boolean shouldDestroy = !success;
+        if(success) {
+            _pool.add(new SoftReference(obj, refQueue));
+            notifyAll(); // _numActive has changed
+        }
+
+        if(shouldDestroy) {
+            try {
+                _factory.destroyObject(obj);
+            } catch(Exception e) {
+                // ignored
+            }
+        }
     }
 
     /** Returns an approximation not less than the of the number of idle instances in the pool. */
     public synchronized int getNumIdle() {
-        assertOpen();
         pruneClearedReferences();
         return _pool.size();
     }
 
+    /**
+     * Return the number of instances currently borrowed from this pool.
+     *
+     * @return the number of instances currently borrowed from this pool
+     */
     public synchronized int getNumActive() {
-        assertOpen();
         return _numActive;
     }
 
+    /**
+     * Clears any objects sitting idle in the pool.
+     */
     public synchronized void clear() {
-        assertOpen();
         if(null != _factory) {
             Iterator iter = _pool.iterator();
             while(iter.hasNext()) {
@@ -167,11 +233,18 @@ public class SoftReferenceObjectPool extends BaseObjectPool implements ObjectPoo
 
     public synchronized void close() throws Exception {
         clear();
-        _pool = null;
-        _factory = null;
         super.close();
     }
 
+    /**
+     * Sets the {@link PoolableObjectFactory factory} this pool uses
+     * to create new instances. Trying to change
+     * the <code>factory</code> while there are borrowed objects will
+     * throw an {@link IllegalStateException}.
+     *
+     * @param factory the {@link PoolableObjectFactory} used to create new instances.
+     * @throws IllegalStateException when the factory cannot be set at this time
+     */
     public synchronized void setFactory(PoolableObjectFactory factory) throws IllegalStateException {
         assertOpen();
         if(0 < getNumActive()) {

commit 6c11df4602cacb5b62a6f1c87529b16d4e7d1705

Fixed an error introduced in r385746 (post-1.3 changes to improve exception management). When passivate throws an exception while returning an object, after destroying the instance, numActive needs to be decremented.
git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/branches/1_4_RELEASE_BRANCH@607117 13f79535-47bb-0310-9956-ffa450edef68

src/java/org/apache/commons/pool/impl/GenericObjectPool.java
@@ -1082,6 +1082,13 @@ public class GenericObjectPool extends BaseObjectPool implements ObjectPool {
                 } catch (Exception e2) {
                     // swallowed
                 }
+                // TODO: Correctness here depends on control in addObjectToPool.
+                // These two methods should be refactored, removing the 
+                // "behavior flag",decrementNumActive, from addObjectToPool.
+                synchronized(this) {
+                    _numActive--;
+                    notifyAll();
+                }
             }
         }
     }

src/test/org/apache/commons/pool/TestObjectPool.java
@@ -273,14 +273,19 @@ public abstract class TestObjectPool extends TestCase {
 
         //// Test exception handling of returnObject
         reset(pool, factory, expectedMethods);
-
+        pool.addObject();
+        pool.addObject();
+        pool.addObject();
+        assertEquals(3, pool.getNumIdle());
         // passivateObject should swallow exceptions and not add the object to the pool
-        idleCount = pool.getNumIdle();
         obj = pool.borrowObject();
+        Object obj2 = pool.borrowObject();
+        assertEquals(1, pool.getNumIdle());
+        assertEquals(2, pool.getNumActive());
         clear(factory, expectedMethods);
         factory.setPassivateObjectFail(true);
         pool.returnObject(obj);
-       // StackObjectPool, SoftReferenceObjectPool also validate on return
+        // StackObjectPool, SoftReferenceObjectPool also validate on return
         if (pool instanceof StackObjectPool || 
                 pool instanceof SoftReferenceObjectPool) {
             expectedMethods.add(new MethodCall(
@@ -289,7 +294,8 @@ public abstract class TestObjectPool extends TestCase {
         expectedMethods.add(new MethodCall("passivateObject", obj));
         removeDestroyObjectCall(factory.getMethodCalls()); // The exact timing of destroyObject is flexible here.
         assertEquals(expectedMethods, factory.getMethodCalls());
-        assertEquals(idleCount, pool.getNumIdle());
+        assertEquals(1, pool.getNumIdle());   // Not returned
+        assertEquals(1, pool.getNumActive()); // But not in active count
 
         // destroyObject should swallow exceptions too
         reset(pool, factory, expectedMethods);