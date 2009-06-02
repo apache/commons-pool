647c0998e7d700c05fe25f02a540c040eb3408fa
Mark Emlyn David Thomas <markt@apache.org>
2009-06-02 21:01:34 +0000

    Anything that clears the pool is likely to mess up the eviction cursors. This fixes the issue sebb reported with testEvictorVisiting() on the dev list.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@781166 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/pool/impl/GenericKeyedObjectPool.java
+++ src/java/org/apache/commons/pool/impl/GenericKeyedObjectPool.java
@@ -1291,6 +1291,9 @@ public class GenericKeyedObjectPool extends BaseKeyedObjectPool implements Keyed
                 _totalInternalProcessing =
                     _totalInternalProcessing + pool.queue.size();
             }
+            // Reset the eviction cursors as clearing the objects will have
+            // invalidated the current cursors
+            resetEvictionKeyCursor();
         }
         destroy(toDestroy);
     }
@@ -1352,6 +1355,9 @@ public class GenericKeyedObjectPool extends BaseKeyedObjectPool implements Keyed
                 itemsToRemove--;
             }
 
+            // Reset the eviction cursors as clearing the objects will have
+            // invalidated the current cursors
+            resetEvictionKeyCursor();
         }
         destroy(toDestroy);
     }
@@ -1376,6 +1382,10 @@ public class GenericKeyedObjectPool extends BaseKeyedObjectPool implements Keyed
             _totalInternalProcessing =
                 _totalInternalProcessing + pool.queue.size();
             toDestroy.put(key, pool.queue);
+            
+            // Reset the eviction cursors as clearing the objects may have
+            // invalidated the current cursors
+            resetEvictionKeyCursor();
         }
         destroy(toDestroy);
     }
@@ -1385,6 +1395,11 @@ public class GenericKeyedObjectPool extends BaseKeyedObjectPool implements Keyed
      * ObjectTimestampPair.value
      * 
      * @param m Map containing keyed pools to clear
+
+     * Implementation note: This is called just outside a sync block where
+     * objects are removed from the pool for destruction. This process is likely
+     * to invalidate any eviction cursors. Therefore, the last call inside the
+     * sync block should be to resetEvictionKeyCursor().
      */
     private void destroy(Map m) {
         for (Iterator keys = m.keySet().iterator(); keys.hasNext();) {
@@ -1710,6 +1725,7 @@ public class GenericKeyedObjectPool extends BaseKeyedObjectPool implements Keyed
                     }
                 }
                 _factory = factory;
+                resetEvictionKeyCursor();
             }
         }
         destroy(toDestroy);
