781fbbf6fd5ba38b10da577d521146be15ea0dbf
Simone Tripodi <simonetripodi@apache.org>
2010-10-28 19:31:17 +0000

    reconfigure() methods made 'synchronized final' modified reconfigure() javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@1028443 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/pool2/impl/StackKeyedObjectPoolFactory.java
+++ src/java/org/apache/commons/pool2/impl/StackKeyedObjectPoolFactory.java
@@ -58,11 +58,12 @@ public class StackKeyedObjectPoolFactory<K,V> implements KeyedObjectPoolFactory<
     }
 
     /**
-     * Reconfigure the current StackObjectPoolFactory instance.
+     * Allows reconfiguring the current StackObjectPoolFactory instance
+     * without setting the parameters one by one.
      *
      * @param config the {@link StackObjectPoolConfig} used to configure the pool.
      */
-    public void reconfigure(StackObjectPoolConfig config) {
+    public synchronized final void reconfigure(StackObjectPoolConfig config) {
         if (config == null) {
             throw new IllegalArgumentException("config must not be null");
         }
