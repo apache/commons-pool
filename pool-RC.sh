f73c2dd1489a6abfb8b29f987a48a70a8ed709a7
Phil Steitz <psteitz@apache.org>
2011-03-30 05:40:25 +0000

    Tagged 1.5.6 RC2.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/tags/POOL_1_5_6_RC2@1086846 13f79535-47bb-0310-9956-ffa450edef68

--- pool-RC.sh
+++ pool-RC.sh
@@ -23,7 +23,7 @@
 # -----------------------------------------------------------------------------
 # Set script variables
 version=1.5.6
-rc=1
+rc=2
 repo_path=~/.m2/repository/commons-pool/commons-pool/${version}
 release_path=~/pool-${version}-rc${rc}
 #
