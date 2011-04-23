2875bec2e8ce3f4088f9f637c2c8ae7bcbb8aa92
Simone Tripodi <simonetripodi@apache.org>
2011-04-23 18:11:25 +0000

    moved package (and related subpackage) to org.apache.commons.pool2
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@1096200 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/pool2/impl/TestStackKeyedObjectPoolFactory.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.pool2.impl;
+
+import org.apache.commons.pool2.KeyedObjectPoolFactory;
+import org.apache.commons.pool2.KeyedPoolableObjectFactory;
+import org.apache.commons.pool2.TestKeyedObjectPoolFactory;
+import org.apache.commons.pool2.impl.StackKeyedObjectPool;
+import org.apache.commons.pool2.impl.StackKeyedObjectPoolFactory;
+
+/**
+ * Tests for {@link StackKeyedObjectPoolFactory}.
+ *
+ * @author Sandy McArthur
+ * @version $Revision$ $Date$
+ */
+public class TestStackKeyedObjectPoolFactory extends TestKeyedObjectPoolFactory {
+    public TestStackKeyedObjectPoolFactory(final String name) {
+        super(name);
+    }
+
+    protected KeyedObjectPoolFactory makeFactory(final KeyedPoolableObjectFactory objectFactory) throws UnsupportedOperationException {
+        return new StackKeyedObjectPoolFactory(objectFactory);
+    }
+
+    public void testConstructors() throws Exception {
+        StackKeyedObjectPoolFactory factory = new StackKeyedObjectPoolFactory();
+        factory.createPool().close();
+
+        factory = new StackKeyedObjectPoolFactory(1);
+        StackKeyedObjectPool pool = (StackKeyedObjectPool)factory.createPool();
+        assertEquals(1,pool._maxSleeping);
+        pool.close();
+
+        factory = new StackKeyedObjectPoolFactory(1, 2);
+        pool = (StackKeyedObjectPool)factory.createPool();
+        assertEquals(1,pool._maxSleeping);
+        assertEquals(2,pool._initSleepingCapacity);
+        pool.close();
+
+        factory = new StackKeyedObjectPoolFactory(createObjectFactory());
+        pool = (StackKeyedObjectPool)factory.createPool();
+        pool.close();
+
+        factory = new StackKeyedObjectPoolFactory(createObjectFactory(),  1);
+        pool = (StackKeyedObjectPool)factory.createPool();
+        assertEquals(1,pool._maxSleeping);
+        pool.close();
+
+        factory = new StackKeyedObjectPoolFactory(createObjectFactory(),  1, 2);
+        pool = (StackKeyedObjectPool)factory.createPool();
+        assertEquals(1,pool._maxSleeping);
+        assertEquals(2,pool._initSleepingCapacity);
+        pool.close();
+
+    }
+}
