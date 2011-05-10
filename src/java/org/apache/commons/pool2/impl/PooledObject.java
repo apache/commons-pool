485fb4439072a83fafe6efe943a47205419c28f0
Mark Thomas <markt@apache.org>
2011-05-10 15:48:22 +0000

    Move to using LinkedBlockingDeque for the queue of idle objects.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@1101516 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/pool2/impl/PooledObject.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.pool2.impl;
+
+/**
+ * This wrapper is used to track the additional information, such as state, for
+ * the pooled objects.
+ */
+public class PooledObject<T> {
+
+    private T _object = null;
+    private volatile PooledObjectState _state = PooledObjectState.IDLE;
+    private long _lastActiveTime = System.currentTimeMillis();
+
+    public PooledObject(T object) {
+        _object = object;
+    }
+    
+    /**
+     * Obtain the underlying object that is wrapped by this instance of
+     * {@link PooledObject}.
+     */
+    public T getObject() {
+        return _object;
+    }
+
+    /**
+     * Obtain the time in milliseconds since this object was last active.
+     */
+    public long getIdleTimeMillis() {
+        return System.currentTimeMillis() - _lastActiveTime;
+    }
+
+    /**
+     * Provides a String form of the wrapper for debug purposes. The format is
+     * not fixed and may change at any time.
+     */
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("Object: ");
+        result.append(_object.toString());
+        result.append(", State: ");
+        result.append(_state.toString());
+        return result.toString();
+    }
+
+    public synchronized boolean startEvictionTest() {
+        if (_state == PooledObjectState.IDLE) {
+            _state = PooledObjectState.MAINTAIN_EVICTION;
+            return true;
+        }
+        
+        return false;
+    }
+
+    public synchronized boolean endEvictionTest() {
+        if (_state == PooledObjectState.MAINTAIN_EVICTION) {
+            _state = PooledObjectState.IDLE;
+            return true;
+        }
+        
+        return false;
+    }
+}
