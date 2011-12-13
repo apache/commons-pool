4f0cb732bc0794cd301fed98d7f17be8d32c60e4
Sebastian Bazley <sebb@apache.org>
2011-12-13 18:29:19 +0000

    Non-final locks are not very safe; make lock objects final
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@1213830 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/pool2/impl/GenericKeyedObjectPool.java
+++ src/java/org/apache/commons/pool2/impl/GenericKeyedObjectPool.java
@@ -2127,8 +2127,9 @@ public class GenericKeyedObjectPool<K,T> implements KeyedObjectPool<K,T>,
     private final LinkedList<Long> activeTimes = new LinkedList<Long>();
     private final LinkedList<Long> idleTimes = new LinkedList<Long>();
     private final LinkedList<Long> waitTimes = new LinkedList<Long>();
-    private Object maxBorrowWaitTimeMillisLock = new Object();
-    private volatile long maxBorrowWaitTimeMillis = 0;
+
+    private final Object maxBorrowWaitTimeMillisLock = new Object();
+    private volatile long maxBorrowWaitTimeMillis = 0; // @GuardedBy("maxBorrowWaitTimeMillisLock")
 
     private ObjectName oname = null;
 
