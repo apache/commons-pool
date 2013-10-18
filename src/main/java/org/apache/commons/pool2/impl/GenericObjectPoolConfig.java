8562576a529b0e13ca4960ce28b5d2dd9366b4b8
Mark Thomas <markt@apache.org>
2013-10-18 09:50:46 +0000

    More Checkstyle inspired Javadoc fixes
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@1533373 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/pool2/impl/GenericObjectPoolConfig.java
+++ src/main/java/org/apache/commons/pool2/impl/GenericObjectPoolConfig.java
@@ -31,19 +31,20 @@ package org.apache.commons.pool2.impl;
 public class GenericObjectPoolConfig extends BaseObjectPoolConfig {
 
     /**
-     * The default maximum number of instances under management
-     * (idle or checked out).
+     * The default value for the {@code maxTotal} configuration attribute.
+     * @see GenericObjectPool#getMaxTotal()
      */
     public static final int DEFAULT_MAX_TOTAL = 8;
 
     /**
-     * The default cap on the number of "sleeping" instances in the pool.
+     * The default value for the {@code maxIdle} configuration attribute.
+     * @see GenericObjectPool#getMaxIdle()
      */
     public static final int DEFAULT_MAX_IDLE = 8;
 
     /**
-     * The default minimum number of "sleeping" instances in the pool before
-     * before the evictor thread (if active) spawns new objects.
+     * The default value for the {@code minIdle} configuration attribute.
+     * @see GenericObjectPool#getMinIdle()
      */
     public static final int DEFAULT_MIN_IDLE = 0;
 
@@ -54,28 +55,82 @@ public class GenericObjectPoolConfig extends BaseObjectPoolConfig {
 
     private int minIdle = DEFAULT_MIN_IDLE;
 
+    /**
+     * Get the value for the {@code maxTotal} configuration attribute
+     * for pools created with this configuration instance.
+     *
+     * @return  The current setting of {@code maxTotal} for this
+     *          configuration instance
+     *
+     * @see GenericObjectPool#getMaxTotal()
+     */
     public int getMaxTotal() {
         return maxTotal;
     }
 
+    /**
+     * Set the value for the {@code maxTotal} configuration attribute for
+     * pools created with this configuration instance.
+     *
+     * @param maxTotal The new setting of {@code maxTotal}
+     *        for this configuration instance
+     *
+     * @see GenericObjectPool#setMaxTotal(int)
+     */
     public void setMaxTotal(int maxTotal) {
         this.maxTotal = maxTotal;
     }
 
 
+    /**
+     * Get the value for the {@code maxIdle} configuration attribute
+     * for pools created with this configuration instance.
+     *
+     * @return  The current setting of {@code maxIdle} for this
+     *          configuration instance
+     *
+     * @see GenericObjectPool#getMaxIdle()
+     */
     public int getMaxIdle() {
         return maxIdle;
     }
 
+    /**
+     * Set the value for the {@code maxIdle} configuration attribute for
+     * pools created with this configuration instance.
+     *
+     * @param maxIdle The new setting of {@code maxIdle}
+     *        for this configuration instance
+     *
+     * @see GenericObjectPool#setMaxIdle(int)
+     */
     public void setMaxIdle(int maxIdle) {
         this.maxIdle = maxIdle;
     }
 
 
+    /**
+     * Get the value for the {@code minIdle} configuration attribute
+     * for pools created with this configuration instance.
+     *
+     * @return  The current setting of {@code minIdle} for this
+     *          configuration instance
+     *
+     * @see GenericObjectPool#getMinIdle()
+     */
     public int getMinIdle() {
         return minIdle;
     }
 
+    /**
+     * Set the value for the {@code minIdle} configuration attribute for
+     * pools created with this configuration instance.
+     *
+     * @param minIdle The new setting of {@code minIdle}
+     *        for this configuration instance
+     *
+     * @see GenericObjectPool#setMinIdle(int)
+     */
     public void setMinIdle(int minIdle) {
         this.minIdle = minIdle;
     }
