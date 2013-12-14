1fe31e7607d647414f018005a48f605af66a6848
Phil Steitz <psteitz@apache.org>
2013-12-14 22:57:24 +0000

    Updated examples to work with pool2 APIs. Added example sources.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@1550982 13f79535-47bb-0310-9956-ffa450edef68

---
+++ doc/ReaderUtil.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.pool2.ObjectPool;
+
+/**
+ * Maintains a pool of StringBuffers used to dump contents of Readers.
+ */
+public class ReaderUtil {
+
+    private ObjectPool<StringBuffer> pool;
+
+    public ReaderUtil(ObjectPool<StringBuffer> pool) {
+        this.pool = pool;
+    }
+
+    /**
+     * Dumps the contents of the {@link Reader} to a String, closing the {@link Reader} when done.
+     */
+    public String readToString(Reader in)
+        throws IOException {
+        StringBuffer buf = null;
+        try {
+            buf = pool.borrowObject();
+            for (int c = in.read(); c != -1; c = in.read()) {
+                buf.append((char) c);
+            }
+            return buf.toString();
+        } catch (IOException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to borrow buffer from pool" + e.toString());
+        } finally {
+            try {
+                in.close();
+            } catch (Exception e) {
+                // ignored
+            }
+            try {
+                if (null != buf) {
+                    pool.returnObject(buf);
+                }
+            } catch (Exception e) {
+                // ignored
+            }
+        }
+    }
+}
