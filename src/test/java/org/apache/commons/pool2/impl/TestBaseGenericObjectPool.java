c96e9ecd658fc29a157851efb17f63535ba9e1a6
Phil Steitz <psteitz@apache.org>
2014-09-28 02:38:14 +0000

    Added some tests for BaseGenericObjectPool stats.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@1628042 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/pool2/impl/TestBaseGenericObjectPool.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.pool2.impl;
+
+import org.apache.commons.pool2.impl.TestGenericObjectPool.SimpleFactory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @version $Revision:$
+ */
+public class TestBaseGenericObjectPool {
+
+    BaseGenericObjectPool<String> pool = null;
+    SimpleFactory factory = null;
+
+    @Before
+    public void setUp() throws Exception {
+        factory = new SimpleFactory();
+        pool = new GenericObjectPool<String>(factory);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        pool.close();
+        pool = null;
+        factory = null; 
+    }
+    
+    @Test
+    public void testBorrowWaitStatistics() {
+    	DefaultPooledObject<String> p = (DefaultPooledObject<String>) factory.makeObject();
+    	pool.updateStatsBorrow(p, 10);
+    	pool.updateStatsBorrow(p, 20);
+    	pool.updateStatsBorrow(p, 20);
+    	pool.updateStatsBorrow(p, 30);
+    	Assert.assertEquals(20, pool.getMeanBorrowWaitTimeMillis(), Double.MIN_VALUE);
+    	Assert.assertEquals(30, pool.getMaxBorrowWaitTimeMillis(), 0);
+    }
+    
+    @Test
+    public void testActiveTimeStatistics() {
+    	for (int i = 0; i < 99; i++) {  // must be < MEAN_TIMING_STATS_CACHE_SIZE
+    		pool.updateStatsReturn(i);
+    	}
+    	Assert.assertEquals(49, pool.getMeanActiveTimeMillis(), Double.MIN_VALUE);
+    }
+}
