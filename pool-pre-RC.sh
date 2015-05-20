2b1fa3e75328a4527aa200db3f2e6534186dd5d9
Phil Steitz <psteitz@apache.org>
2015-05-20 13:36:25 +0000

    Update for 2.4.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@1680566 13f79535-47bb-0310-9956-ffa450edef68

--- pool-pre-RC.sh
+++ pool-pre-RC.sh
@@ -25,6 +25,6 @@
 #
 # $Revision$ $Date$
 # ----------------------------------------------------------------------------
-version=2.3
+version=2.4
 mvn changes:announcement-generate -Prelease-notes -Dchanges.version=${version}
 mvn commons:download-page -Dcommons.componentid=pool  -Dcommons.release.version=${version}
