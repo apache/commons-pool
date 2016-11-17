fe4945387e2fbc9ba2a9ac4eb7c1fa1d50f40310
Gary D. Gregory <ggregory@apache.org>
2016-11-17 00:02:16 +0000

    Add starter TravisCI configuration. Will pick up Travis profile from new parent module when released.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/pool/trunk@1770094 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/pool2/PoolTest.java
@@ -0,0 +1,80 @@
+package org.apache.commons.pool2;
+
+import static org.junit.Assert.assertFalse;
+
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class PoolTest {
+    private static final CharSequence COMMONS_POOL_EVICTIONS_TIMER_THREAD_NAME = "commons-pool-EvictionTimer";
+    private static final long EVICTION_PERIOD_IN_MILLIS = 100;
+
+    private static class Foo {
+    }
+
+    private static class PooledFooFactory implements PooledObjectFactory<Foo> {
+        private static final long VALIDATION_WAIT_IN_MILLIS = 1000;
+
+        @Override
+        public PooledObject<Foo> makeObject() throws Exception {
+            return new DefaultPooledObject<Foo>(new Foo());
+        }
+
+        @Override
+        public void destroyObject(PooledObject<Foo> pooledObject) throws Exception {
+        }
+
+        @Override
+        public boolean validateObject(PooledObject<Foo> pooledObject) {
+            try {
+                Thread.sleep(VALIDATION_WAIT_IN_MILLIS);
+            } catch (final InterruptedException e) {
+                Thread.interrupted();
+            }
+            return false;
+        }
+
+        @Override
+        public void activateObject(PooledObject<Foo> pooledObject) throws Exception {
+        }
+
+        @Override
+        public void passivateObject(PooledObject<Foo> pooledObject) throws Exception {
+        }
+    }
+
+    @Test
+    public void testPool() throws Exception {
+        final GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        poolConfig.setTestWhileIdle(true /* testWhileIdle */);
+        final PooledFooFactory pooledFooFactory = new PooledFooFactory();
+        GenericObjectPool<Foo> pool = null;
+        try {
+            pool = new GenericObjectPool<Foo>(pooledFooFactory, poolConfig);
+            pool.setTimeBetweenEvictionRunsMillis(EVICTION_PERIOD_IN_MILLIS);
+            pool.addObject();
+            try {
+                Thread.sleep(EVICTION_PERIOD_IN_MILLIS);
+            } catch (final InterruptedException e) {
+                Thread.interrupted();
+            }
+        } finally {
+            if (pool != null) {
+                pool.close();
+            }
+        }
+        final Thread[] threads = new Thread[Thread.activeCount()];
+        Thread.enumerate(threads);
+        for (final Thread thread : threads) {
+            if (thread == null) {
+                continue;
+            }
+            final String name = thread.getName();
+            assertFalse(name, name.contains(COMMONS_POOL_EVICTIONS_TIMER_THREAD_NAME));
+        }
+    }
+}
