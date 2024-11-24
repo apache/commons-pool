7cf0e1224ae3b5225ca391a8bbd2406d1d35d0f1
Gary Gregory <garydgregory@gmail.com>
2024-11-24 08:46:31 -0500

    Update single line comment

--- src/main/java/org/apache/commons/pool3/impl/GenericObjectPool.java
+++ src/main/java/org/apache/commons/pool3/impl/GenericObjectPool.java
@@ -561,7 +561,7 @@ public class GenericObjectPool<T, E extends Exception> extends BaseGenericObject
                 }
             }
 
-            // Do not block more if maxWaitTimeMillis is set.
+            // Do not block more if localMaxWaitDuration is set.
             if (create == null && localMaxWaitDuration.compareTo(Duration.ZERO) > 0 &&
                     Duration.between(localStartInstant, Instant.now()).compareTo(localMaxWaitDuration) >= 0) {
                 create = Boolean.FALSE;
