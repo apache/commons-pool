7478d56858e60b0c342e5c30ea89e02b9522e992
Gary D. Gregory <garydgregory@gmail.com>
2025-02-11 08:06:33 -0500

    Add Checkstyle UnusedImports

--- src/test/java/org/apache/commons/pool2/impl/TestSoftRefOutOfMemory.java
+++ src/test/java/org/apache/commons/pool2/impl/TestSoftRefOutOfMemory.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
