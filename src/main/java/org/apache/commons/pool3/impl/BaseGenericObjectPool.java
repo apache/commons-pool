0f19cee9de9ef3ee9741a7345456832bd4d5ab07
Gary Gregory <garydgregory@gmail.com>
2025-05-01 16:49:26 -0400

    Javadoc

--- src/main/java/org/apache/commons/pool3/impl/BaseGenericObjectPool.java
+++ src/main/java/org/apache/commons/pool3/impl/BaseGenericObjectPool.java
@@ -1121,8 +1121,9 @@ public abstract class BaseGenericObjectPool<T, E extends Exception> extends Base
     }
 
     /**
-     * Tests whether this pool instance been closed.
-     * @return {@code true} when this pool has been closed.
+     * Tests whether this pool instance is closed.
+     *
+     * @return {@code true} when this pool is closed.
      */
     public final boolean isClosed() {
         return closed;
